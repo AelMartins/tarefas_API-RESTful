becaf26911778b97e98500d90c7b290226933e6c
AelMartins <oneapcwork@gmail.com>
2023-04-20 18:30:14 -0300

    developed and tested app.post to create new tasks

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 // Program uses .gitignore to ignore nodemon modules
-
 // Necessary installation through the terminal using npm.
--- index.js
+++ index.js
@@ -5,3 +5,28 @@ app.use(express.json());
 app.listen(8080, () => {
     console.log("O SERVIDOR ESTÁ ATIVO NA PORTA 8080");
 })
+
+
+let tarefa = [];
+
+app.post('/novaTarefa', (req, res) => {
+    const { titulo, descricao } = req.body;
+    if (!titulo) {
+        return res.status(400).send('ERRO, TÍTULO OBRIGATÓRIO!');
+    }
+    const novaTarefa = {
+        id: tarefa.length + 1,
+        titulo: titulo,
+        descricao: descricao,
+        concluida: false
+    }
+    tarefa.push(novaTarefa);
+    let print = `<h1> CRIADA TAREFA: "${titulo}" COM ID: "${novaTarefa.id}" </h1>`;
+    print += `<h1> DESCRIÇÃO: "${descricao}" </h1>`;
+    if (novaTarefa.concluida === false){
+        print += `<h1> TAREFA PENDENTE </h1>`;
+    } else {
+        print += `<h1> TAREFA CONCLUIDA </h1>`;
+    }
+    res.send(print);
+})
