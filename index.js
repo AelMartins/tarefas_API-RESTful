29ab188b2466730e2084af2dbcc4369eb9e93389
AelMartins <oneapcwork@gmail.com>
2023-04-21 16:11:39 -0300

    added delete function

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.listen(8080, () => {
 let tarefas = [];
 let tarefa_Id = 0;
 
+// POST
+
 app.post('/novaTarefa', (req, res) => {
     const { titulo, descricao } = req.body;
     if (!titulo) {
@@ -26,7 +28,7 @@ app.post('/novaTarefa', (req, res) => {
         concluida: false
     }
     tarefas.push(novaTarefa);
-    let print = `CRIADA TAREFA: " ${titulo} " COM ID: " ${novaTarefa.id} " | `;
+    let print = `| CRIADA TAREFA: " ${titulo} " COM ID: " ${novaTarefa.id} " | `;
     print += `DESCRIÇÃO: " ${descricao} " | `;
     if (novaTarefa.concluida === false){
         print += `TAREFA PENDENTE `;
@@ -36,11 +38,13 @@ app.post('/novaTarefa', (req, res) => {
     res.send(print);
 })
 
+// PUTs
+
 app.put('/concluirTarefa/:id', (req, res) => {
     const { id } = req.params;
     const tarefaEncontrada = tarefas.find(callbackTarefa => callbackTarefa.id === parseInt(id));
     if (!tarefaEncontrada) {
-        return res.status(404).send(`TAREFA COM ID " ${id} " NÃO ENCONTRADA!`);
+        return res.status(404).send(`TAREFA COM ID " ${id} " NÃO FOI ENCONTRADA!`);
     }
     tarefaEncontrada.concluida = true;
     res.send(`A TAREFA " ${tarefaEncontrada.titulo} " COM ID " ${tarefaEncontrada.id} " FOI CONCLUÍDA!`);
@@ -50,7 +54,7 @@ app.put('/desconcluirTarefa/:id', (req, res) => {
     const { id } = req.params;
     const tarefaEncontrada = tarefas.find(callbackTarefa => callbackTarefa.id === parseInt(id));
     if (!tarefaEncontrada) {
-        return res.status(404).send(`TAREFA COM ID " ${id} " NÃO ENCONTRADA!`);
+        return res.status(404).send(`TAREFA COM ID " ${id} " NÃO FOI ENCONTRADA!`);
     }
     tarefaEncontrada.concluida = false;
     res.send(`A TAREFA " ${tarefaEncontrada.titulo} " COM ID " ${tarefaEncontrada.id} " FOI DESCONCLUÍDA!`);
@@ -60,13 +64,15 @@ app.put('/atualizarTarefa', (req, res) => {
     const { id, titulo, descricao } = req.body;
     const atualizarTarefa = tarefas.find(callbackTarefa => callbackTarefa.id === id);
     if (!atualizarTarefa) {
-        return res.status(404).send(`A TAREFA COM ID " ${id} " NÃO ENCONTRADA!`);
+        return res.status(404).send(`A TAREFA COM ID " ${id} " NÃO FOI ENCONTRADA!`);
     }
     atualizarTarefa.titulo = titulo;
     atualizarTarefa.descricao = descricao;
     res.send(`TAREFA ATUALIZADA COM SUCESSO!`);
     console.log(tarefas);
-});
+})
+
+// GETs
 
 app.get('/pegaTarefas', (req, res) => {
     const tarefasString = JSON.stringify(tarefas, null, 2);
@@ -77,7 +83,25 @@ app.get('/pegaTarefas', (req, res) => {
 app.get('/pegaTarefa', (req, res) => {
     const { id } = req.body;
     const pegarTarefa = tarefas.find(callbackTarefa => callbackTarefa.id === id);
+    if (!pegarTarefa) {
+        return res.status(404).send(`A TAREFA COM ID " ${id} " NÃO FOI ENCONTRADA!`);
+    }
     const tarefaString = JSON.stringify(pegarTarefa, null, 2);
     const printHTML = `ESTA É A TAREFA REQUISITADA: <pre>${tarefaString}</pre>`;
+    res.send(printHTML);
+})
+
+// DELETE
+
+app.delete('/deletarTarefa', (req, res) => {
+    const { id } = req.body;
+    const deletaTarefa = tarefas.find(callbackTarefa => callbackTarefa.id === id);
+    if (!deletaTarefa) {
+        return res.status(404).send(`A TAREFA COM ID " ${id} " NÃO FOI ENCONTRADA!`);
+    }
+    tarefas.splice(id);
+    let printHTML = `TAREFA DELETADA COM SUCESSO! `
+    const tarefasString = JSON.stringify(tarefas, null, 2);
+    printHTML += `SEGUE A LISTA DE TODAS AS SUAS TAREFAS AINDA EXISTENTES: <pre>${tarefasString}</pre>`;
     res.send(printHTML);
 });
